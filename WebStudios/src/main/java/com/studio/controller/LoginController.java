0fde8df8d915606430d3654c716ec2c866980058
pepe <pedroctbr@gmail.com>
2019-06-07 04:22:49 -0500

    lOGIN ON  CLICK

--- WebStudios/src/main/java/com/studio/controller/LoginController.java
+++ WebStudios/src/main/java/com/studio/controller/LoginController.java
@@ -3,13 +3,21 @@ package com.studio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.studio.model.User;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	String home() {
-		System.out.println("login");
-		return "#principal";
+	public @ResponseBody User getShopInJSON() {
+
+		User shop = new User();
+		shop.setUser("ppe");
+		shop.setPassword("123");
+
+		return shop;
+
 	}
 }
--- WebStudios/src/main/webapp/js/App.js
+++ WebStudios/src/main/webapp/js/App.js
@@ -48,49 +48,83 @@ app.controller("CtrlOracle", [ "$scope", function($scope) {
 	$scope.titulo = "Oracle";
 } ]);
 
-// function Login() {
-// this.login = function() {
-// return this.ancho * this.alto;
-// }
-// }
+//function RemoteResource($http, baseUrl) {
+//	this.get = function(fnOK, fnError) {
+//		$http({
+//			method : 'GET',
+//			url : baseUrl + '/login'
+//		}).success(function(data, status, headers, config) {
+//			fnOK(data);
+//		}).error(function(data, status, headers, config) {
+//			fnError(data, status);
+//		});
+//	}
+//}
 //
-// function RemoteResource($http, baseUrl) {
-// this.get = function(fnOK, fnError) {
-// $http({
-// method : 'GET',
-// url : baseUrl + '/login'
-// }).success(function(data, status, headers, config) {
-// fnOK(data);
-// }).error(function(data, status, headers, config) {
-// fnError(data, status);
-// });
-// }
-// }
+//function RemoteResourceProvider() {
+//	var _baseUrl;
+//	this.setBaseUrl = function(baseUrl) {
+//		_baseUrl = baseUrl;
+//	}
+//	this.$get = [ '$http', function($http) {
+//		return new RemoteResource($http, _baseUrl);
+//	} ];
+//}
 //
-// function RemoteResourceProvider() {
-// var _baseUrl;
-// this.setBaseUrl = function(baseUrl) {
-// _baseUrl = baseUrl;
-// }
-// this.$get = [ '$http', function($http) {
-// return new RemoteResource($http, _baseUrl);
-// } ];
-// }
+//app.constant("baseUrl", "http://localhost:8081/WebStudios");
+//app.config([ 'baseUrl', 'remoteResourceProvider',
+//		function(baseUrl, remoteResourceProvider) {
+//			remoteResourceProvider.setBaseUrl(baseUrl);
+//		} ]);
 //
-// app.constant("baseUrl", "http://localhost:8081/WebStudios");
-// app.config([ 'baseUrl', 'remoteResourceProvider',
-// function(baseUrl, remoteResourceProvider) {
-// remoteResourceProvider.setBaseUrl(baseUrl);
-// } ]);
+//app.provider("remoteResource", RemoteResourceProvider);
 //
-// app.provider("remoteResource", RemoteResourceProvider);
+//app.controller("CtrlLogin", [ '$scope', 'remoteResource',
+//		function($scope, remoteResource) {
+//			remoteResource.get(function() {
+//				$scope.seguro = seguro;
+//			}, function(data, status) {
+//				alert("Ha fallado la petición. Estado HTTP:" + status);
+//			});
 //
-// app.controller("CtrlLogin", [ '$scope', 'remoteResource',
-// function($scope, remoteResource) {
-// remoteResource.get(function() {
-// $scope.seguro = seguro;
-// }, function(data, status) {
-// alert("Ha fallado la petición. Estado HTTP:" + status);
-// });
+//			console.log("logn consile");
 //
-// } ]);
+//		} ]);
+//
+app.factory("idioma", function() {
+	return "es-es";
+});
+
+app.factory("matematicas_simples", function() {
+	return {
+		sumar : function(a, b) {
+			return a + b;
+		},
+		restar : function(a, b) {
+			return a - b;
+		}
+	}
+});
+
+app.factory("radio", function() {
+	return 10;
+});
+
+app.factory("area", function() {
+	return function(radio) {
+		return 3.1416 * radio * radio;
+	}
+});
+
+ app.controller("CtrlLogin", [ "$scope", "idioma", "matematicas_simples",
+		"radio", "area",
+		function($scope, idioma, matematicas_simples, radio, area) {
+			$scope.idioma = idioma;
+			$scope.suma = matematicas_simples.sumar(3, 6);
+			$scope.area = area(radio);
+
+			$scope.myFunc = function() {
+				console.log("function called");
+			};
+
+		} ]);
